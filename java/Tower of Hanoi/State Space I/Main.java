04d27a65bbe571cc12ec77a4630a752d24662ae0
Péter <versenyi98@gmail.com>
2020-03-02 20:16:10 +0100

    Add comments

--- java/Tower of Hanoi/State Space I/Main.java
+++ java/Tower of Hanoi/State Space I/Main.java
@@ -5,6 +5,7 @@ class Main {
     private static char state[] = {'A', 'A', 'A'};
 
     private static boolean isValid(char state[]) {
+        // Ha az egyik korong nem az A, B vagy C oszlopon van, akkor rossz állapot
         for (int i = 0; i < 3; i++) {
             if (state[i] != 'A' && state[i] != 'B' && state[i] != 'C') {
                 return false;
@@ -14,6 +15,7 @@ class Main {
     }
 
     private static boolean isGoal(char state[]) {
+        // Ha valamelyik korong nem a C oszlopon van, akkor nem célállapot
         for (int i = 0; i < 3; i++) {
             if (state[i] != 'C') {
                 return false;
@@ -24,15 +26,16 @@ class Main {
 
     private static boolean opReq(int n, char m, char state[]) {
 
-        if (n < 0 || n > 3) return false;
+        if (n < 0 || n > 2) return false;
         if (m < 'A' || m > 'C') return false;
+        // ha ugyanoda raknánk ahol most van akkor helytelen operátor
         if (state[n] == m) return false;
 
         for (int i = 0; i < 3; i++) {
-            if (state[i] == m && i < n)
-                return false;
-            if (state[i] == state[n] && i < n)
-                return false;
+            // ha a cél oszlopon van kisebb korong akkor nem jó operátor
+            if (state[i] == m && i < n) return false;
+            // ha a jelen oszlopon van kisebb korong akkor nem jó operátor
+            if (state[i] == state[n] && i < n) return false;
         }
 
         return true;
@@ -53,7 +56,6 @@ class Main {
         Scanner scanner = new Scanner(System.in);
 
         do {
-
             System.out.print("Mit: ");
             n = scanner.nextInt();
 
